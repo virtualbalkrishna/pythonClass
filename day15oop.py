2d0a22362addc47e89008f3bd4a898b0649563f2
Balkrishna Thami <balkrishnathami739@gmail.com>
2024-07-08 19:33:20 +0545

    First code

---
+++ day15oop.py
@@ -0,0 +1,14 @@
+#OOP: Object Oriented Programming. In OOP we use and object to
+#design computer programs
+#Class: Blueprint to creat Object
+#Object: Instance of Class
+### How to create class
+class student:
+    def_init__(self, name, age):
+    self.age = age
+    def display():
+        print("Name is {self.name}" )
+        print("Name is {self.name}" )
+
+    ### how to creat object
+    st1 = Student(name="Balkrishna",age="Thami")
